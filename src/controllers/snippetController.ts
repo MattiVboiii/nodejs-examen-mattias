e7683716493715d5e6d91dc78795a21d22185947
Mattias Verlinden <mattiverlinden@hotmail.com>
2025-03-12 11:13:50 +0100

    enhance getSnippets route with pagination and filtering options

--- src/controllers/snippetController.ts
+++ src/controllers/snippetController.ts
@@ -18,11 +18,39 @@ export const createSnippet = async (req: Request, res: Response) => {
 
 export const getSnippets = async (req: Request, res: Response) => {
   try {
-    const snippets = await Snippet.find();
-    const decodedSnippets = snippets.map((snippet) => {
+    const {
+      page = 1,
+      limit = 10,
+      sort = 'createdAt',
+      order = 'desc',
+    } = req.query;
+
+    // Find snippets based on query parameters
+    const snippets = await Snippet.find({
+      ...(req.query.language && {
+        language: { $eq: req.query.language },
+      }),
+      ...(req.query.tags
+        ? {
+            tags: {
+              $all: (req.query.tags as string).split(','),
+            },
+          }
+        : {}),
+    }).sort({ [sort as string]: order === 'desc' ? -1 : 1 });
+
+    // Paginate the snippets
+    const paginatedSnippets = snippets.slice(
+      (+page - 1) * +limit,
+      +page * +limit
+    );
+
+    // Decode the code for each snippet
+    const decodedSnippets = paginatedSnippets.map((snippet) => {
       const decodedCode = Buffer.from(snippet.code, 'base64').toString('utf-8');
       return { ...snippet.toObject(), code: decodedCode };
     });
+
     res.status(200).json({ status: 'success', data: decodedSnippets });
   } catch (error: unknown) {
     if (error instanceof Error) {
